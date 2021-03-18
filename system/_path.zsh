955bf3b3c514ad88712f9b14e0a60e39906936ad
Donald Piret <donald@donaldpiret.com>
2021-03-18 14:52:47 +0800

    fix: path issue

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$PATH:$ZSH/bin:./bin:/usr/local/bin:/usr/local/sbin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
