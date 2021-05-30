de8f7b68eb06a70a4305384a38d65bea4838d91d
Donald Piret <donaldpiret@users.noreply.github.com>
2021-05-30 18:18:35 +0800

    feat: add yarn global bin to the path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$PATH:$ZSH/bin:./bin:/usr/local/bin:/usr/local/sbin"
+export PATH="$PATH:$ZSH/bin:$(yarn global bin):./bin:/usr/local/bin:/usr/local/sbin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
